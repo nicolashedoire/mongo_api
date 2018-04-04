a0f79bd83d8b411466a181292c1de1c3aca8b34f
Nicolas.hedoire <nicolashedoire@hotmail.com>
2018-04-04 21:15:18 +0200

    check if account exists

--- controllers/login-controller.js
+++ controllers/login-controller.js
@@ -1,11 +1,22 @@
 const FacebookAccount = require('../models/facebookAccount');
 
-module.exports = {    
-    connect (req,res) {
-        let account = new FacebookAccount(req.body).save().then(() => {
-            res.send({
-                status : '200'
-            });
-        })
+module.exports = {
+    connect(req, res) {
+        // console.log(req.body);
+        let account = new FacebookAccount(req.body);
+
+        FacebookAccount.find({ id : account.id }).then((response) => {
+            if (response.length) {
+                res.send({
+                    status: 'ALREADY_EXISTS'
+                });
+            } else {
+                account.save().then(() => {
+                    res.send({
+                        status: '200'
+                    });
+                })
+            }
+        });
     },
 };
