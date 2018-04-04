e63e4e1668b1ffcd85ae46ca60fdec9e79e9d8b7
Nicolas.hedoire <nicolashedoire@hotmail.com>
2018-04-04 09:54:18 +0200

    change server port and install nodemon dependency

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ server.set('json spaces', 2);
 routes(server);
 
 server.listen(4000, () =>{
-    console.log("Serveur demarré en écoute sur le porte 3050 !")
-    mongoose.connect('mongodb://localhost/user_api_database');
+    console.log("Serveur demarré en écoute sur le porte 4000")
+    mongoose.connect('mongodb://localhost/friends_finder');
     mongoose.connection
     .once('open',() => console.log("Connexion à MongoDB établie !"))
     .on('error',(error) => {
