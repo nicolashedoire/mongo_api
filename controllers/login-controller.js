f8bad7a5cfa556fe50340bc359dad5de7ac063a3
Nicolas.hedoire <nicolashedoire@hotmail.com>
2018-04-05 17:52:26 +0200

    add jwt and send token in auth

--- controllers/login-controller.js
+++ controllers/login-controller.js
@@ -1,18 +1,28 @@
 const Account = require('../models/accounts');
+const jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
+
+function generateToken() {
+  return jwt.sign({ user: true }, 'APLMDJKN481DJNOIJSOKPCO48', { expiresIn: '1h' });
+}
 
 module.exports = {
-    connect(req, res) {
-        // console.log(req.body);
-        let account = new Account(req.body);
+  connect(req, res) {
+    // console.log(req.body);
+    let account = new Account(req.body);
 
-        account.save().then(() => {
-            res.send({
-                status: '200'
-            });
-        }, (err) => {
-            res.send({
-                status: 'ALREADY_EXISTS'
-            });
-        })
-    },
+    account.save().then(
+      () => {
+        res.send({
+          status: '200',
+          token: generateToken()
+        });
+      },
+      err => {
+        res.send({
+          status: 'ALREADY_EXISTS',
+          token: generateToken()
+        });
+      }
+    );
+  }
 };
--- server.js
+++ server.js
@@ -4,11 +4,16 @@ const bodyParser = require('body-parser');
 const server = express();
 const mongoose = require('mongoose');
 const cors = require('cors');
+const jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
 
 mongoose.Promise = global.Promise;
 
+const secret = 'APLMDJKN481DJNOIJSOKPCO48';
+
 server.use(bodyParser.json());
 server.set('json spaces', 2);
+
+server.set('superSecret', secret); // secret variable
 
 // Allow access control origin
 server.use(cors())
