44f8413daae673024110c7746a1f8322916f7a4f
Nicolas.hedoire <nicolashedoire@hotmail.com>
2018-04-12 15:44:30 +0200

    add places

--- controllers/place.controller.js
+++ controllers/place.controller.js
@@ -1,19 +1,78 @@
 places = [
     {
+        id: 1,
         city: 'Lille',
         name: 'b148'
     },
     {
+        id: 2,
         city: 'Lille',
         name: 'la plage'
     },
     {
+        id: 3,
         city: 'Lille',
         name: 'la plage 2'
     },
     {
+        id: 4,
         city: 'Lille',
         name: 'le kremlin'
+    },
+    {
+        id: 5,
+        city: 'Lille',
+        name: 'la capsule'
+    },
+    {
+        id: 6,
+        city: 'Lille',
+        name: 'le dernier bar avant la fin du monde'
+    },
+    {
+        id: 7,
+        city: 'Lille',
+        name: 'le bar braz'
+    },
+    {
+        id: 8,
+        city: 'Lille',
+        name: 'le dansy'
+    },
+    {
+        id: 9,
+        city: 'Lille',
+        name: 'le bar parallèle'
+    },
+    {
+        id: 10,
+        city: 'Lille',
+        name: 'le bartown'
+    },
+    {
+        id: 11,
+        city: 'Lille',
+        name: 'le beerstro'
+    },
+    {
+        id: 12,
+        city: 'Lille',
+        name: 'le tir na nog'
+    },
+    {
+        id: 13,
+        city: 'Lille',
+        name: 'l\'irlandais'
+    },
+    {
+        id: 14,
+        city: 'Lille',
+        name: 'le café oz the australian bar'
+    },
+    {
+        id: 15,
+        city: 'Lille',
+        name: 'la pirogue'
     },
 ];
 
