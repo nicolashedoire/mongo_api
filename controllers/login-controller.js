fcddd94ab5c1c1f5b9c2b213c845dec13955bd7e
Nicolas.hedoire <nicolashedoire@hotmail.com>
2018-04-17 15:40:34 +0200

    change payload for login

--- controllers/login-controller.js
+++ controllers/login-controller.js
@@ -1,26 +1,37 @@
 const Account = require('../models/accounts');
 const jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
 
-function generateToken() {
-  return jwt.sign({ user: true }, 'APLMDJKN481DJNOIJSOKPCO48', { expiresIn: '1h' });
+function generateToken(user) {
+  const token = jwt.sign(
+    {
+      role: 'admin',
+      email: user.email,
+      id: user.id,
+      image: user.image,
+      name: user.name
+    },
+    'APLMDJKN481DJNOIJSOKPCO48',
+    { expiresIn: '1h' }
+  );
+  return token;
 }
 
 module.exports = {
   connect(req, res) {
     // console.log(req.body);
     let account = new Account(req.body);
-
+    
     account.save().then(
       () => {
         res.send({
           status: '200',
-          token: generateToken()
+          token: generateToken(account)
         });
       },
       err => {
         res.send({
           status: 'ALREADY_EXISTS',
-          token: generateToken()
+          token: generateToken(account)
         });
       }
     );
