5e2e4165859fc38be193312107527769e9fca02a
Nicolas.hedoire <nicolashedoire@hotmail.com>
2018-05-22 15:40:04 +0200

    update isActive when activities.length === 0

--- controllers/activity-controller.js
+++ controllers/activity-controller.js
@@ -6,13 +6,18 @@ const ObjectId = require('mongoose').ObjectID;
 
 module.exports = {
   readAll(req, res) {
-    Account.findOne({ id: req.query.userId })
+    Account.findOne({
+        id: req.query.userId
+      })
       .populate('activities')
       .then(user => {
-        res.send({ activities: user.activities });
+        res.send({
+          activities: user.activities
+        });
       });
   },
   readAllToday(req, res) {
+
     var startDate = new Date();
     startDate.setHours(0, 0, 0, 0);
     var endDate = new Date();
@@ -21,23 +26,27 @@ module.exports = {
     endDate.setHours(0, 0, 0, 0);
 
     Activity.find({
-      date: {
-        $gte: startDate,
-        $lte: endDate
-      }
-    })
+        user: req.query.userId,
+        date: {
+          $gte: startDate,
+          $lte: endDate
+        }
+      })
       .populate('user')
       .populate('users')
       .then(results => {
         // show the admins in the past month
-        console.log(results);
         res.send(results);
       });
   },
   read(req, res) {
     const id = req.params.id;
-    Activity.findById({ _id: id }).then(activity => {
-      res.send({ activity });
+    Activity.findById({
+      _id: id
+    }).then(activity => {
+      res.send({
+        activity
+      });
     });
   },
   getByPlaceId(req, res) {
@@ -50,15 +59,17 @@ module.exports = {
 
     const id = req.params.id;
     Activity.find({
-      placeId: id,
-      date: {
-        $gte: startDate,
-        $lte: endDate
-      }
-    })
+        placeId: id,
+        date: {
+          $gte: startDate,
+          $lte: endDate
+        }
+      })
       .populate('user')
       .then(activities => {
-        res.send({ activities: activities });
+        res.send({
+          activities: activities
+        });
       });
   },
   create(req, res) {
@@ -68,9 +79,13 @@ module.exports = {
     const id = req.body.placeId;
     placeName = '';
 
-    Bar.find({ id: id }).then(bar => {
+    Bar.find({
+      id: id
+    }).then(bar => {
       placeName = bar[0].name;
-      Account.findOne({ id: req.body.userId }).then(user => {
+      Account.findOne({
+        id: req.body.userId
+      }).then(user => {
         const activity = new Activity({
           label: label,
           placeName: placeName,
@@ -82,10 +97,14 @@ module.exports = {
         user.isActive = true;
         user.save().then(() => {
           activity.save().then(() => {
-            Account.findOne({ id: req.body.userId })
+            Account.findOne({
+                id: req.body.userId
+              })
               .populate('activities')
               .then(user => {
-                res.send({ activities: user.activities });
+                res.send({
+                  activities: user.activities
+                });
               });
           });
         });
@@ -95,13 +114,17 @@ module.exports = {
   join(req, res) {
     const activityId = req.params.id;
     const user = req.body;
-    Account.find({ id: user.id }).then(user => {
+    Account.find({
+      id: user.id
+    }).then(user => {
       Activity.findById(activityId)
         .populate('users')
         .populate('user')
         .then(activity => {
           if (activity.user.id === user[0].id) {
-            res.send({ status: 'NOT_ALLOWED' });
+            res.send({
+              status: 'NOT_ALLOWED'
+            });
             return;
           }
           for (let i = 0; i < activity.users.length; i++) {
@@ -135,12 +158,36 @@ module.exports = {
       .populate('user')
       .then(activity => {
         let userId = activity.user._id;
-        Account.update({ _id: userId }, { $pull: { activities: id } }).then(
+        Account.update({
+          _id: userId
+        }, {
+          $pull: {
+            activities: id
+          }
+        }).then(
           () => {
-            Activity.findByIdAndRemove({ _id: id }).then(activity => {
-              res.send({
-                status: 200
-              });
+            Activity.findByIdAndRemove({
+              _id: id
+            }).then(activity => {
+
+              Account.findById(userId).then((user) => {
+
+                console.log(user);
+                if (user.activities.length === 0) {
+                  user.isActive = false;
+
+                  user.save().then(() => {
+
+                    res.send({
+                      status: 200
+                    });
+                  })
+                } else {
+                  res.send({
+                    status: 200
+                  });
+                }
+              })
             });
           },
           err => {
